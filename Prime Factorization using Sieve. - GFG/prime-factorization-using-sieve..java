665fa28d766e8606d35f3dfcd5d2dbde187a7948
Romick Kundu <romickkundu5555@gmail.com>
2023-05-06 12:29:04 +0530

    Added solution - LeetHub

---
+++ Prime Factorization using Sieve. - GFG/prime-factorization-using-sieve..java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            Solution obj = new Solution();
+            obj.sieve();
+            List<Integer> ans = obj.findPrimeFactors(n);
+            for (int e : ans) {
+                System.out.print(e + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // You must implement this function
+    static void sieve() {}
+
+    static List<Integer> findPrimeFactors(int N) {
+        // code here
+        List<Integer> res=new ArrayList<Integer>();
+        
+        for(int i=2;i<=Math.sqrt(N);i++){
+            while(N%i==0){
+                res.add(i);
+                N/=i;
+            }
+        }
+        if(N>1)res.add(N);
+        return res;
+    }
+}
