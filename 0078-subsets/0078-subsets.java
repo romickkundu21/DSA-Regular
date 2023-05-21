210352182c94b1a715cf1279b7d938b6c6c9c3fa
Romick Kundu <romickkundu5555@gmail.com>
2023-05-21 19:57:23 +0530

    Time: 1 ms (74.66%), Space: 42.1 MB (99.33%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -4,7 +4,7 @@ class Solution {
         List<List<Integer>> res=new ArrayList<>();
         res.add(new ArrayList<>());
         for(int i=0;i<nums.length;i++){
-            int size = res.size();
+            int size=res.size();
             for(int j=0;j<size;j++){
                 List<Integer> arr=new ArrayList<>(res.get(j));
                 arr.add(nums[i]);
