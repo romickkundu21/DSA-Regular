e80d913a09ab94f8aa8be96a32e7d75f044e95e3
Romick Kundu <romickkundu5555@gmail.com>
2023-06-03 21:59:00 +0530

    Time: 0 ms (100.00%), Space: 44.4 MB (7.58%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        if(image[sr][sc]==color)return image;
+        fill(image, sr, sc, image[sr][sc], color);
+        return image;
+    }
+
+    public static void fill(int[][] image, int sr, int sc, int color, int newColor){
+        if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length || image[sr][sc]!=color){
+            return;
+        }
+        image[sr][sc]=newColor;
+        fill(image,sr+1,sc,color,newColor);
+        fill(image,sr-1,sc,color,newColor);
+        fill(image,sr,sc+1,color,newColor);
+        fill(image,sr,sc-1,color,newColor);
+    }
+}
