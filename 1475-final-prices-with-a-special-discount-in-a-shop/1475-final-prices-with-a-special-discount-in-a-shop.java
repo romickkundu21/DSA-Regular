e5200d55384b1918971a780aa0702bc99281d517
Romick Kundu <romickkundu5555@gmail.com>
2023-06-14 15:21:18 +0530

    Time: 4 ms (42.59%), Space: 43.5 MB (41.05%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        Stack<Integer> s=new Stack<>();
+        for(int i=0;i<prices.length;i++){
+            while(!s.isEmpty() && prices[i]<=prices[s.peek()]){
+                prices[s.pop()]-=prices[i];
+            }
+            s.push(i);
+        }
+        return prices;
+    }
+}
