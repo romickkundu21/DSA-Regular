b3c7ddfe8439e20c2175c06b0a45d034034b32bc
Romick Kundu <romickkundu5555@gmail.com>
2023-06-23 19:12:28 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Detect cycle in an undirected graph/detect-cycle-in-an-undirected-graph.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.isCycle(V, adj);
+            if (ans)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        boolean b[]=new boolean[V];
+        for(int i=0;i<V;i++){
+            if(!b[i]){
+                if(dfs(i,adj,b,-1))return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean dfs(int V, ArrayList<ArrayList<Integer>> adj, boolean b[], int parent){
+        b[V]=true;
+        for(int i:adj.get(V)){
+            if(!b[i]){
+                if(dfs(i,adj,b,V))return true;
+            }
+                else if(parent!=i){
+                    return true;
+                }
+        }
+        return false;
+    }
+}
